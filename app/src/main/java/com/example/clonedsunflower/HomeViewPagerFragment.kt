70893930500af8afbd556aa11097cafb98a79cd3
강한빛 <fchopinof99@gmail.com>
2022-05-07 17:46:16 +0900

    Implement view pager

--- app/src/main/java/com/example/clonedsunflower/HomeViewPagerFragment.kt
+++ app/src/main/java/com/example/clonedsunflower/HomeViewPagerFragment.kt
@@ -5,7 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import com.example.clonedsunflower.adapters.SunflowerPagerAdapter
 import com.example.clonedsunflower.databinding.FragmentHomeViewPagerBinding
+import com.google.android.material.tabs.TabLayoutMediator
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -15,6 +18,14 @@ class HomeViewPagerFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val binding = FragmentHomeViewPagerBinding.inflate(inflater, container, false)
+
+        binding.viewPager.adapter = SunflowerPagerAdapter(this)
+
+        TabLayoutMediator(binding.tabLayout, binding.viewPager) { tab, position ->
+            tab.text = if(position == 0) "0" else "1"
+        }.attach()
+
+        (activity as AppCompatActivity).setSupportActionBar(binding.toolbar)
 
         return binding.root
     }
