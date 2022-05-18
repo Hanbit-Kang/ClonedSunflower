9e36c928fb25b91e5b9409da019004a95302ca9f
강한빛 <fchopinof99@gmail.com>
2022-05-18 11:10:51 +0900

    Add RunWith annotation

--- app/src/androidTest/java/com/example/clonedsunflower/data/PlantDaoTest.kt
+++ app/src/androidTest/java/com/example/clonedsunflower/data/PlantDaoTest.kt
@@ -15,7 +15,7 @@ import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
-// @RunWith(AndroidJUnit4::class)
+@RunWith(AndroidJUnit4::class)
 class PlantDaoTest {
     private lateinit var database: AppDatabase
     private lateinit var plantDao: PlantDao
